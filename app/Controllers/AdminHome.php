1bacc760783fef24cc72ebec165afd160dc4d9a4
adilsanak <adilsaputra0101@gmail.com>
2022-05-01 15:14:20 +0700

    Init

--- app/Controllers/AdminHome.php
+++ app/Controllers/AdminHome.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class AdminHome extends BaseController
 {
     public function index()
     {
-        return view('view_home');
+        return view('admin/view_home');
     }
 }
